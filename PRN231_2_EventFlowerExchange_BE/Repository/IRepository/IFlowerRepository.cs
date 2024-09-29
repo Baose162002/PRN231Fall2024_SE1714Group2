68971a037543eb785c2b68f56c1892f2b69edfa7
duc-anh-le <ducanhle.iter@gmail.com>
2024-09-29 18:18:05 +0700

    lda: CRUD for Flower

---
+++ PRN231_2_EventFlowerExchange_BE/Repository/IRepository/IFlowerRepository.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.IRepository
+{
+    public interface IFlowerRepository
+    {
+        Task<List<Flower>> GetAllFlowers();
+        Task<Flower> GetFlowerById(int id);
+        Task Create(Flower flower);
+        Task Update(Flower flower, int id);
+        Task Delete(int id);
+    }
+}
---
+++ PRN231_2_EventFlowerExchange_BE/Repository/Repository/FlowerRepository.cs
@@ -0,0 +1,53 @@
+﻿using BusinessObject;
+using Microsoft.EntityFrameworkCore;
+using Repository.IRepository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Repository.Repository
+{
+    public class FlowerRepository : IFlowerRepository
+    {
+        private readonly FlowerShopContext _context;
+        public FlowerRepository()
+        {
+            _context ??= new FlowerShopContext();
+        }
+
+        public async Task<List<Flower>> GetAllFlowers()
+        {
+            return await _context.Flowers.ToListAsync();
+        }
+
+        public async Task<Flower> GetFlowerById(int id)
+        {
+            return await _context.Flowers.FindAsync(id);
+        }
+
+        public async Task Create(Flower flower)
+        {
+            await _context.Flowers.AddAsync(flower);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(Flower flower, int id)
+        {
+            var existingFlower = await _context.Flowers.FindAsync(id);
+            if (existingFlower != null)
+            {
+                _context.Entry(existingFlower).CurrentValues.SetValues(flower);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(int id)
+        {
+            var flower = await _context.Flowers.FindAsync(id); // Truy cập trực tiếp vào DbSet
+            if (flower != null)
+            {
+                _context.Flowers.Remove(flower); // Truy cập trực tiếp vào DbSet
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
