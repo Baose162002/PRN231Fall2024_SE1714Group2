0d446d21610b62e6d7e394ea81e62832f0e1d512
Tran Minh Quan <steammicharel@gmail.com>
2024-09-30 23:43:11 +0700

    CRUD Review

---
+++ PRN231_2_EventFlowerExchange_BE/BusinessObject/DTO/Response/ListReviewDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.DTO.Response
+{
+    public class ListReviewDTO
+    {
+        public int ReviewId { get; set; }
+        public int Rating { get; set; }
+        public string Feedback { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public int BatchId { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
---
+++ PRN231_2_EventFlowerExchange_BE/Service/Service/ReviewService.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using BusinessObject.DTO.Request;
+using BusinessObject.DTO.Response;
+using BusinessObject;
+using Repository.IRepository;
+using Service.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Service
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IMapper _mapper;
+
+        public ReviewService(IReviewRepository reviewRepository, IMapper mapper)
+        {
+            _reviewRepository = reviewRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ListReviewDTO>> GetAllReviews()
+        {
+            var reviews = await _reviewRepository.GetAllReviews();
+            return _mapper.Map<List<ListReviewDTO>>(reviews);
+        }
+
+        public async Task<ListReviewDTO> GetReviewById(int id)
+        {
+            var review = await _reviewRepository.GetReviewById(id);
+            return _mapper.Map<ListReviewDTO>(review);
+        }
+
+        public async Task<List<ListReviewDTO>> GetReviewsByBatchId(int batchId)
+        {
+            var reviews = await _reviewRepository.GetReviewsByBatchId(batchId);
+            return _mapper.Map<List<ListReviewDTO>>(reviews);
+        }
+
+        public async Task<List<ListReviewDTO>> GetReviewsByCustomerId(int customerId)
+        {
+            var reviews = await _reviewRepository.GetReviewsByCustomerId(customerId);
+            return _mapper.Map<List<ListReviewDTO>>(reviews);
+        }
+
+        public async Task CreateReview(CreateReviewDTO createReviewDTO)
+        {
+            var review = _mapper.Map<Review>(createReviewDTO);
+            review.ReviewDate = DateTime.Now;
+            await _reviewRepository.Create(review);
+        }
+
+        public async Task UpdateReview(int id, UpdateReviewDTO updateReviewDTO)
+        {
+            var existingReview = await _reviewRepository.GetReviewById(id);
+            if (existingReview == null)
+            {
+                throw new ArgumentException("Review not found");
+            }
+
+            _mapper.Map(updateReviewDTO, existingReview);
+            await _reviewRepository.Update(existingReview);
+        }
+
+        public async Task DeleteReview(int id)
+        {
+            await _reviewRepository.Delete(id);
+        }
+    }
+}
