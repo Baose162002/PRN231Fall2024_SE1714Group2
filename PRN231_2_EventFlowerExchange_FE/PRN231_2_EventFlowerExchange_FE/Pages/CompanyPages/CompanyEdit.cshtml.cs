b38cf6534bfbab45d2477bbd258e565f940231d5
Ân Lê Thiên <thienanse.work@gmail.com>
2024-10-05 15:26:00 +0700

    Base CRUD for Company FE

---
+++ PRN231_2_EventFlowerExchange_FE/PRN231_2_EventFlowerExchange_FE/Pages/CompanyPages/CompanyEdit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject;
+
+namespace PRN231_2_EventFlowerExchange_FE.Pages.CompanyPages
+{
+    public class EditModel : PageModel
+    {
+        private readonly BusinessObject.FlowerShopContext _context;
+
+        public EditModel(BusinessObject.FlowerShopContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Company Company { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var company =  await _context.Companies.FirstOrDefaultAsync(m => m.CompanyId == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            Company = company;
+           ViewData["UserId"] = new SelectList(_context.Users, "UserId", "Address");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Company).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompanyExists(Company.CompanyId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CompanyExists(int id)
+        {
+            return _context.Companies.Any(e => e.CompanyId == id);
+        }
+    }
+}
