2888fdbfe0e9c8a8f372ec5dc318c4e5f0298928
duc-anh-le <ducanhle.iter@gmail.com>
2024-10-16 11:51:28 +0700

    CRUD for delivery

---
+++ PRN231_2_EventFlowerExchange_BE/BusinessObject/DTO/Response/ListDeliveryDTO.cs
@@ -0,0 +1,20 @@
+﻿using BusinessObject.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.DTO.Response
+{
+    public class ListDeliveryDTO
+    {
+        public int DeliveryId { get; set; }
+        public EnumList.DeliveryStatus DeliveryStatus { get; set; }
+        public string ReasonNote { get; set; }
+        public DateTime PickupTime { get; set; }
+        public DateTime DeliveryTime { get; set; }
+        public int OrderId { get; set; }
+        public int DeliveryPersonnelId { get; set; }
+    }
+}
---
+++ PRN231_2_EventFlowerExchange_BE/WebApi_EventFlowerExchange/Controllers/DeliveryController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using BusinessObject.DTO.Request;
+using Microsoft.AspNetCore.Mvc;
+using Service.IService;
+using Service.Service;
+
+namespace WebApi_EventFlowerExchange.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeliveryController : ControllerBase
+    {
+        private readonly IDeliveryService _deliveryService;
+        private readonly IMapper _mapper;
+
+        public DeliveryController(IDeliveryService deliveryService, IMapper mapper)
+        {
+            _deliveryService = deliveryService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllDeliveries()
+        {
+            var deliveries = _deliveryService.GetAllDeliveries();
+            return Ok(deliveries);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetDeliveryById(int id)
+        {
+            var delivery = _deliveryService.GetDeliveryById(id);
+            if (delivery == null)
+            {
+                return NotFound();
+            }
+            return Ok(delivery);
+        }
+
+        [HttpPost]
+        public IActionResult CreateDelivery([FromBody] CreateDeliveryDTO createDeliveryDTO)
+        {
+            _deliveryService.CreateDelivery(createDeliveryDTO);
+            return CreatedAtAction(nameof(GetDeliveryById), new { id = createDeliveryDTO.OrderId }, createDeliveryDTO);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateDelivery([FromBody] UpdateDeliveryDTO updateDeliveryDTO, int id)
+        {
+            _deliveryService.UpdateDelivery(updateDeliveryDTO, id);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteDelivery(int id)
+        {
+            _deliveryService.DeleteDelivery(id);
+            return NoContent();
+        }
+    }
+}
