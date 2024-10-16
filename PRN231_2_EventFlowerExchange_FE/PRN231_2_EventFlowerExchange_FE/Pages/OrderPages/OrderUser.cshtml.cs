9062fe59ce70e864d82647b44abb4aefab697c47
Tran Minh Quan <steammicharel@gmail.com>
2024-10-16 15:15:12 +0700

    FE fix order index

--- PRN231_2_EventFlowerExchange_FE/PRN231_2_EventFlowerExchange_FE/Pages/OrderPages/OrderUser.cshtml.cs
+++ PRN231_2_EventFlowerExchange_FE/PRN231_2_EventFlowerExchange_FE/Pages/OrderPages/OrderUser.cshtml.cs
@@ -1,18 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using BusinessObject.DTO.Response;
-using Newtonsoft.Json;
 
 namespace PRN231_2_EventFlowerExchange_FE.Pages.OrderPages
 {
-    public class DetailsModel : PageModel
+    public class OrderUser : PageModel
     {
         private readonly HttpClient _httpClient;
         private readonly string _baseApiUrl;
 
-        public DetailsModel(HttpClient httpClient, IConfiguration configuration)
+        public OrderUser(HttpClient httpClient, IConfiguration configuration)
         {
             _httpClient = httpClient;
             _baseApiUrl = configuration["ApiSettings:BaseUrl"];
@@ -22,17 +22,14 @@ namespace PRN231_2_EventFlowerExchange_FE.Pages.OrderPages
 
         public async Task<IActionResult> OnGetAsync(int id)
         {
-            // Gọi API để lấy thông tin chi tiết của đơn hàng
             var response = await _httpClient.GetAsync($"{_baseApiUrl}/api/order/{id}");
-
             if (response.IsSuccessStatusCode)
             {
-                var jsonResponse = await response.Content.ReadAsStringAsync();
-                Order = JsonConvert.DeserializeObject<ListOrderDTO>(jsonResponse);
+                Order = await response.Content.ReadFromJsonAsync<ListOrderDTO>();
             }
             else
             {
-                return NotFound("Order not found");
+                return NotFound("Order not found.");
             }
 
             return Page();
