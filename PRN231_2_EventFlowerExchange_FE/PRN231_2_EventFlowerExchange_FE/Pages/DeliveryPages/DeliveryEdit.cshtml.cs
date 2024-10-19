56d178bd1c13f8ea50f590dee3e069877d8df3ff
duc-anh-le <ducanhle.iter@gmail.com>
2024-10-19 16:35:31 +0700

    Add FE for Delivery

---
+++ PRN231_2_EventFlowerExchange_FE/PRN231_2_EventFlowerExchange_FE/Pages/DeliveryPages/DeliveryEdit.cshtml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject;
+
+namespace PRN231_2_EventFlowerExchange_FE.Pages.DeliveryPages
+{
+    public class EditModel : PageModel
+    {
+        private readonly BusinessObject.FlowerShopContext _context;
+
+        public EditModel(BusinessObject.FlowerShopContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Delivery Delivery { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var delivery =  await _context.Deliveries.FirstOrDefaultAsync(m => m.DeliveryId == id);
+            if (delivery == null)
+            {
+                return NotFound();
+            }
+            Delivery = delivery;
+           ViewData["DeliveryPersonnelId"] = new SelectList(_context.Users, "UserId", "Address");
+           ViewData["OrderId"] = new SelectList(_context.Orders, "OrderId", "DeliveryAddress");
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Delivery).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DeliveryExists(Delivery.DeliveryId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool DeliveryExists(int id)
+        {
+            return _context.Deliveries.Any(e => e.DeliveryId == id);
+        }
+    }
+}
