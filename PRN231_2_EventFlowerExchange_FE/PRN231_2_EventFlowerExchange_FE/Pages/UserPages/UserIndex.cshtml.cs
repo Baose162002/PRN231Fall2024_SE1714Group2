e5468b6e642678c38c40b69694d3a202192b1d31
Tran Minh Quan <steammicharel@gmail.com>
2024-11-02 15:24:48 +0700

    FE: FIx Order Notification when Pay

--- PRN231_2_EventFlowerExchange_FE/PRN231_2_EventFlowerExchange_FE/Pages/UserPages/UserIndex.cshtml.cs
+++ PRN231_2_EventFlowerExchange_FE/PRN231_2_EventFlowerExchange_FE/Pages/UserPages/UserIndex.cshtml.cs
@@ -32,10 +32,13 @@ namespace PRN231_2_EventFlowerExchange_FE.Pages.UserPages
         {
 
             var token = HttpContext.Session.GetString("JWTToken");
+            var userRole = HttpContext.Session.GetString("UserRole");
 
-            if (string.IsNullOrEmpty(token))
+            // Check if the user is authenticated and has the "Admin" role
+            if (string.IsNullOrEmpty(token) || userRole != "Admin")
             {
-                return RedirectToPage("/Login/Login");
+                ModelState.AddModelError(string.Empty, "Unauthorized access. Only Admins can view this page.");
+                return RedirectToPage("/Index");
             }
 
             if (pageNumber.HasValue && pageNumber > 0)
